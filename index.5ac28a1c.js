86d3bf46f1fd680f27c63fff6804d129ad95c107
nnalinn <alina.vysotskayaaa@gmail.com>
2025-02-09 00:19:53 +0200

    make build

---
+++ index.5ac28a1c.js
@@ -0,0 +1,2 @@
+document.querySelector("table").querySelectorAll("tr").forEach(function(e){var l=e.cells;if(l.length>1){var r=l[1].cloneNode(!0);e.insertBefore(r,l[l.length-1])}});
+//# sourceMappingURL=index.5ac28a1c.js.map
