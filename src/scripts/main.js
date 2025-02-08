5149de294764419c1149421d91361911205cc39a
nnalinn <alina.vysotskayaaa@gmail.com>
2025-02-09 00:08:44 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const table = document.querySelectorAll('tr');
+
+const rows = table.querySelectorAll('tr');
+
+for (let i = 0; i < rows.length; i++) {
+  const row = rows[i];
+  const cells = row.cells;
+
+  const clonedCell = cells[1].cloneNode(true);
+
+  row.insertBefore(clonedCell, cells[cells.length - 1]);
+}
